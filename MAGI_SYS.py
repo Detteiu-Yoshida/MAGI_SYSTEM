cf5b888596bd1bd0ff796da1c5fd1f754569d9bb
Chutoro_Detteiu <57580049+Detteiu-Yoshida@users.noreply.github.com>
2021-04-03 13:23:10 +0900

    容認,否認の表示時に色がつくように変更

--- MAGI_SYS.py
+++ MAGI_SYS.py
@@ -1,13 +1,19 @@
-#MAGI_SYS.py Ver.1.1 Copyright ©️2021 Chutoro Detteiu(@ctr_exe) All Rights Reserved.
+#MAGI_SYS.py Ver.1.2 Copyright ©️2021 Chutoro Detteiu(@ctr_exe) All Rights Reserved.
 import random
 loop_key = 1
 
 priority_class = ['AAA','AA_','A__']
+class Color:
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    END = '\033[0m'
 
 print('MAGI booting...')
 print('MAGI SYSTEM Ver.1.0 Developed by N.Akagi')
 while True:
     if loop_key == 1:
+        magi_output = ''
+        priority = 0
         discussion_num = random.randint(0,9999)
         priority_num = random.randint(0,2)
         start = input('提訴内容を入力してください:')
@@ -19,12 +25,13 @@ while True:
         magis_yn = [int(melchior_output),int(balthasar_output),int(casper_output)]
         magis_name = ['MELCHIOR: ','BALTHASAR: ','CASPER: ']
         yn = ['否認','容認']
+        color_list = [Color.RED,Color.GREEN]
         priority = priority_class[priority_num].count('A')
         magi_output = melchior_output + balthasar_output + casper_output
         for i in range(0,3):
-            print(magis_name[i] + yn[magis_yn[i]])
+            print(magis_name[i] + color_list[magis_yn[i]] + yn[magis_yn[i]] + Color.END)
 
-        if magi_output >= priority:
+        if magi_output >= priority :
             allover_output = '可決'
         else:
             allover_output = '否決'
@@ -32,3 +39,4 @@ while True:
         print('提訴 ' + start + ' は ' + allover_output + ' されました')
         if start == '終了' and allover_output == '可決':
             break
+
