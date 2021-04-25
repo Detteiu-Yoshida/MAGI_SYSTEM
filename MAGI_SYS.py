1ce6eb35e3b19edd3ce9a169d6727d1f82b229f8
Chutoro_Detteiu <57580049+Detteiu-Yoshida@users.noreply.github.com>
2021-04-25 10:23:02 +0900

    表記の変更
    Ver.1.3へ

--- MAGI_SYS.py
+++ MAGI_SYS.py
@@ -1,4 +1,4 @@
-#MAGI_SYS.py Ver.1.2 Copyright ©️2021 Chutoro Detteiu(@ctr_exe) All Rights Reserved.
+#MAGI_SYS.py Ver.1.3 Copyright ©️2021 Chutoro Detteiu(@ctr_exe) All Rights Reserved.
 import random
 loop_key = 1
 
@@ -24,18 +24,18 @@ while True:
         casper_output = random.randint(0,1)#カスパーの回答
         magis_yn = [int(melchior_output),int(balthasar_output),int(casper_output)]#それぞれの回答をまとめる
         magis_name = ['MELCHIOR: ','BALTHASAR: ','CASPER: ']#プリント時の名前
-        yn = ['否認','容認']
-        color_list = [Color.RED,Color.GREEN]#文字色のリスト化(否決時に赤、可決時に緑になるように)
+        yn = ['否定','承認']
+        color_list = [Color.RED,Color.GREEN]#文字色のリスト化(否定時に赤、承認時に緑になるように)
         priority = priority_class[priority_num].count('A')#Aの数(=重要度)の判定(1~3)
         magi_output = melchior_output + balthasar_output + casper_output#マギの回答(1+1+1で最大3)
         for i in range(0,3):#結果の表示
             print(magis_name[i] + color_list[magis_yn[i]] + yn[magis_yn[i]] + Color.END)
 
-        if magi_output >= priority :#可決の数がAの数を超えているか判定
-            allover_output = '可決'
+        if magi_output >= priority :#承認の数がAの数を超えているか判定
+            allover_output = '承認'
         else:
-            allover_output = '否決'
+            allover_output = '否定'
 
         print('提訴 ' + start + ' は ' + allover_output + ' されました')
-        if start == '終了' and allover_output == '可決':#終了判定
+        if start == '終了' and allover_output == '承認':#終了判定
             break
