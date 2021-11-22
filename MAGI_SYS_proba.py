723c4a8c68a41d2ee0634f868813668c1f21e512
Chutoro_Detteiu <57580049+Detteiu-Yoshida@users.noreply.github.com>
2021-11-23 00:34:10 +0900

    微修正
    間違ったコメントの訂正

--- MAGI_SYS_proba.py
+++ MAGI_SYS_proba.py
@@ -11,14 +11,14 @@ while True:
         discussion_num = random.randint(0,9999)#ただの乱数
         start = input('提訴内容を入力してください:')
         print('CODE:' + str(discussion_num))
-        melchior_output = random.randint(0,100)#メルキオールの回答(0,1の二種)
+        melchior_output = random.randint(0,100)#メルキオールの回答
         balthasar_output = random.randint(0,100)#バルタザールの回答
         casper_output = random.randint(0,100)#カスパーの回答
         magis_yn = [int(melchior_output),int(balthasar_output),int(casper_output)]#それぞれの回答をまとめる
         magis_name = ['MELCHIOR: ','BALTHASAR: ','CASPER: ']#プリント時の名前
 
-        magi_output = sum(magis_yn) / len(magis_yn)#マギの回答(1+1+1で最大3)
+        magi_output = sum(magis_yn) / len(magis_yn)#マギの回答(メルキオール,バルタザール,カスパーの算出値の平均)
         for i in range(0,3):#結果の表示
             print(magis_name[i] + str(magis_yn[i]) + "%")
 
-        print('提訴 ' + start + ' の確率は ' + str(magi_output) + ' です')
+        print('提訴 ' + start + ' の確率は ' + str(magi_output) + '% です')
