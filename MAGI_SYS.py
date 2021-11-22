90c416c7693a5c301248da1f99640c5fa892a29b
Chutoro_Detteiu <57580049+Detteiu-Yoshida@users.noreply.github.com>
2021-11-22 18:08:40 +0900

    Ver1.4
    微調整

--- MAGI_SYS.py
+++ MAGI_SYS.py
@@ -1,8 +1,10 @@
-#MAGI_SYS.py Ver.1.3 Copyright ©️2021 Chutoro Detteiu(@ctr_exe) All Rights Reserved.
+#MAGI_SYS.py Ver.1.4 Copyright ©️2021 Chutoro Detteiu(@ctr_exe) All Rights Reserved.
 import random
 loop_key = 1
 
 priority_class = ['AAA','AA_','A__']
+magis_name = ['MELCHIOR: ','BALTHASAR: ','CASPER: ']#プリント時の名前
+yn = ['否定','承認']
 class Color:#文字色指定
     RED = '\033[31m'
     GREEN = '\033[32m'
@@ -23,15 +25,13 @@ while True:
         balthasar_output = random.randint(0,1)#バルタザールの回答
         casper_output = random.randint(0,1)#カスパーの回答
         magis_yn = [int(melchior_output),int(balthasar_output),int(casper_output)]#それぞれの回答をまとめる
-        magis_name = ['MELCHIOR: ','BALTHASAR: ','CASPER: ']#プリント時の名前
-        yn = ['否定','承認']
-        color_list = [Color.RED,Color.GREEN]#文字色のリスト化(否定時に赤、承認時に緑になるように)
+        color_list = [Color.RED,Color.GREEN]#文字色のリスト化(否決時に赤、可決時に緑になるように)
         priority = priority_class[priority_num].count('A')#Aの数(=重要度)の判定(1~3)
         magi_output = melchior_output + balthasar_output + casper_output#マギの回答(1+1+1で最大3)
         for i in range(0,3):#結果の表示
             print(magis_name[i] + color_list[magis_yn[i]] + yn[magis_yn[i]] + Color.END)
 
-        if magi_output >= priority :#承認の数がAの数を超えているか判定
+        if magi_output >= priority :#可決の数がAの数を超えているか判定
             allover_output = '可決'
         else:
             allover_output = '否決'
